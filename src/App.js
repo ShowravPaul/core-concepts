1ba1a03e10c750ab5dbb059580bec26ba33a3b12
ShowravPaul <showravpaul98@gmail.com>
2021-02-24 20:38:02 +0600

    introduction to Reactgit add .

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // array
+  const players = ["Errichto", "Shakib", "CR7", "Messi"];
+  const works = ["cp", "cricket", "football", "football"];
+  // const playerName = players.map(player => player);
+  // console.log(playerName);
+
+  // array of objects
+  const playersObject = [
+    { name: "Errichto", work: "cp" },
+    { name: "Jon", work: "music" },
+    { name: "Petr", work: "cp" }
+  ]
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,17 +23,84 @@ function App() {
         <p>
           Edit done <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>I am a react person.</p>
+
+        <Users></Users>
+
+        <Counter></Counter>
+        <ul>
+          {
+            players.map(player => <li>{player}</li>)
+          }
+          {
+            works.map(work => <li>{work}</li>)
+          }
+        </ul>
+
+        {/* <Person name={players[0]} work={works[0]}></Person> */}
+
+        {/* lets make Person type component for all element of playersObject */}
+        {
+          playersObject.map(element => <Person player={element}></Person>)
+        }
       </header>
     </div>
   );
+}
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  const handleIncrease = () => {
+    setCount(count + 1);
+  };
+  const handleDecrease = () => {
+    setCount(count - 1);
+  };
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={handleIncrease}> Increase </button>
+      {/* <button onClick={handleDecrease}> Decrease </button> */}
+      <button onMouseMove={handleDecrease}> Decrease </button>
+    </div>
+  )
+}
+
+function Users() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/users`)
+      .then(response => response.json())
+      .then(data => setUsers(data));
+  }, [])
+  
+  return (
+    <div>
+      <h3>Dynamic Users: {users.length}</h3>
+      <ul>
+        {
+          users.map(user => <li>{user.name}</li>)
+        }
+      </ul>
+    </div>
+  )
+}
+
+function Person(params) {
+  const personStyle = {
+    border: '2px solid yellow',
+    borderRadius: '10px',
+    backgroundColor: 'Black',
+    margin: '10px 10px',
+    padding: '10px 10px'
+  }
+  //console.log(params);
+  return (
+    <div style={personStyle}>
+      <h1>hello {params.player.name}</h1>
+      <h3>how is  your {params.player.work}?</h3>
+    </div>
+  )
 }
 
 export default App;
